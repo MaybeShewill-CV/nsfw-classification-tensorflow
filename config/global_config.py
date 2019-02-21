db55896d6cbefdc5836a330626cd8dd50674ee17
luoyao <luoyao@baidu.com>
2019-02-21 19:56:01 +0800

    reduce training batch size

--- config/global_config.py
+++ config/global_config.py
@@ -33,9 +33,9 @@ __C.TRAIN.GPU_MEMORY_FRACTION = 0.95
 # Set the GPU allow growth parameter during tensorflow training process
 __C.TRAIN.TF_ALLOW_GROWTH = True
 # Set the shadownet training batch size
-__C.TRAIN.BATCH_SIZE = 48
+__C.TRAIN.BATCH_SIZE = 36
 # Set the shadownet validation batch size
-__C.TRAIN.VAL_BATCH_SIZE = 48
+__C.TRAIN.VAL_BATCH_SIZE = 36
 # Set the learning rate decay steps
 __C.TRAIN.LR_DECAY_STEPS_1 = 40000
 # Set the learning rate decay steps
---
+++ web_client/nsfw_classes.js
@@ -0,0 +1,10 @@
+//
+/**
+ * -*- coding: utf-8 -*-
+ * @Time  : 19-2-21 下午7:45
+ * @Author: Luo Yao
+ * @Site  : http://icode.baidu.com/repos/baidu/personal-code/Luoyao
+ * @File  : nsfw_classes.js.js
+ * @IDE:  : PyCharm
+ * =============================================================================
+ */
