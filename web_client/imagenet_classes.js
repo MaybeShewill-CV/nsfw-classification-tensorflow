f3d7586dfa186b0514e5f252ed29027c9f7d3d2a
luoyao <luoyao@baidu.com>
2019-02-22 23:32:16 +0800

    upload model

---
+++ web_client/imagenet_classes.js
@@ -0,0 +1,9 @@
+/**
+ * -*- coding: utf-8 -*-
+ * @Time  : 19-2-22 下午9:56
+ * @Author: Luo Yao
+ * @Site  : http://icode.baidu.com/repos/baidu/personal-code/Luoyao
+ * @File  : imagenet_classes.js
+ * @IDE:  : PyCharm
+ * ===================================================================
+ */
---
+++ web_client/mobilenet.js
@@ -0,0 +1,9 @@
+/**
+ * -*- coding: utf-8 -*-
+ * @Time  : 19-2-22 下午9:55
+ * @Author: Luo Yao
+ * @Site  : http://icode.baidu.com/repos/baidu/personal-code/Luoyao
+ * @File  : mobilenet.js
+ * @IDE:  : PyCharm
+ * ===================================================================
+ */
