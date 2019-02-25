f7c1ea3f622426c91f0e4a538a9be4aebce6cbe7
luoyao <luoyao@baidu.com>
2019-02-25 14:03:26 +0800

    delete useless files

--- web_client/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# @Time    : 19-2-21 下午7:31
-# @Author  : Luo Yao
-# @Site    : http://icode.baidu.com/repos/baidu/personal-code/Luoyao
-# @File    : __init__.py.py
-# @IDE: PyCharm
