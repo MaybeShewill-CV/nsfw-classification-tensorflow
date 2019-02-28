f2329f44055eda268b189419a4c0532f5f652d2d
luoyao <luoyao@baidu.com>
2019-02-28 16:45:28 +0800

    add some test images example

---
+++ tools/evaluate_nsfw.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Time    : 19-2-28 上午11:20
+# @Author  : Luo Yao
+# @Site    : http://icode.baidu.com/repos/baidu/personal-code/Luoyao
+# @File    : evaluate_nsfw.py
+# @IDE: PyCharm
