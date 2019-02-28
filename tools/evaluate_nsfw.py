133f5b7aba72475f80e9df67e38ff30735b41fe4
luoyao <luoyao@baidu.com>
2019-02-28 16:46:18 +0800

    add model evaluation script

--- tools/evaluate_nsfw.py
+++ tools/evaluate_nsfw.py
@@ -5,3 +5,282 @@
 # @Site    : http://icode.baidu.com/repos/baidu/personal-code/Luoyao
 # @File    : evaluate_nsfw.py
 # @IDE: PyCharm
+"""
+Evaluate nsfw model
+"""
+import itertools
+import os.path as ops
+import argparse
+import glog as log
+
+import tensorflow as tf
+import numpy as np
+from sklearn.metrics import (confusion_matrix, precision_score, recall_score,
+                             precision_recall_curve, average_precision_score, f1_score)
+from sklearn.preprocessing import label_binarize
+from sklearn.utils.fixes import signature
+import matplotlib.pyplot as plt
+
+from config import global_config
+from nsfw_model import nsfw_classification_net
+from data_provider import nsfw_data_feed_pipline
+
+CFG = global_config.cfg
+
+_R_MEAN = 123.68
+_G_MEAN = 116.78
+_B_MEAN = 103.94
+_CHANNEL_MEANS = [_B_MEAN, _G_MEAN, _R_MEAN]
+
+
+def init_args():
+    """
+
+    :return:
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--dataset_dir', type=str, default=None, help='The dataset dir')
+    parser.add_argument('--weights_path', type=str, help='The model weights file path')
+    parser.add_argument('--top_k', type=int, default=1, help='Evaluate top k error')
+
+    return parser.parse_args()
+
+
+def calculate_top_k_error(predictions, labels, k=1):
+    """
+    Calculate the top-k error
+    :param predictions: 2D tensor with shape [batch_size, num_labels]
+    :param labels: 1D tensor with shape [batch_size, 1]
+    :param k: int
+    :return: tensor with shape [1]
+    """
+    batch_size = CFG.TEST.BATCH_SIZE
+    in_top_k = tf.to_float(tf.nn.in_top_k(predictions, labels, k=k))
+    num_correct = tf.reduce_sum(in_top_k)
+
+    return (batch_size - num_correct) / float(batch_size)
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        log.info("Normalized confusion matrix")
+    else:
+        log.info('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+
+
+def plot_precision_recall_curve(labels, predictions_prob, class_nums, average_function='weighted'):
+    """
+    Plot precision recall curve
+    :param labels:
+    :param predictions_prob:
+    :param class_nums:
+    :param average_function:
+    :return:
+    """
+    labels = label_binarize(labels, classes=np.linspace(0, class_nums - 1, num=class_nums).tolist())
+    predictions_prob = np.array(predictions_prob, dtype=np.float32)
+
+    precision = dict()
+    recall = dict()
+    average_precision = dict()
+
+    for i in range(class_nums):
+        precision[i], recall[i], _ = precision_recall_curve(labels[:, i],
+                                                            predictions_prob[:, i])
+        average_precision[i] = average_precision_score(labels[:, i], predictions_prob[:, i])
+
+    # A "micro-average": quantifying score on all classes jointly
+    precision[average_function], recall[average_function], _ = precision_recall_curve(
+        labels.ravel(), predictions_prob.ravel())
+    average_precision[average_function] = average_precision_score(
+        labels, predictions_prob, average=average_function)
+    log.info('Average precision score, {:s}-averaged '
+             'over all classes: {:.5f}'.format(average_function, average_precision[average_function]))
+
+    plt.figure()
+    plt.step(recall[average_function], precision[average_function], color='b', alpha=0.2,
+             where='post')
+    step_kwargs = ({'step': 'post'} if 'step' in signature(plt.fill_between).parameters else {})
+    plt.fill_between(recall[average_function], precision[average_function], alpha=0.2, color='b',
+                     **step_kwargs)
+
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.ylim([0.0, 1.05])
+    plt.xlim([0.0, 1.0])
+    plt.title(
+        'Average precision score, {:s}-averaged over '
+        'all classes: AP={:.5f}'.format(average_function, average_precision[average_function]))
+
+
+def calculate_evaluate_statics(labels, predictions, model_name='Nsfw', avgerage_method='weighted'):
+    """
+    Calculate Precision, Recall and F1 score
+    :param labels:
+    :param predictions:
+    :param model_name:
+    :param avgerage_method:
+    :return:
+    """
+    log.info('Model name: {:s}:'.format(model_name))
+    log.info('\tPrecision: {:.5f}'.format(precision_score(y_true=labels,
+                                                          y_pred=predictions,
+                                                          average=avgerage_method)))
+    log.info('\tRecall: {:.5f}'.format(recall_score(y_true=labels,
+                                                    y_pred=predictions,
+                                                    average=avgerage_method)))
+    log.info('\tF1: {:.5f}\n'.format(f1_score(y_true=labels,
+                                              y_pred=predictions,
+                                              average=avgerage_method)))
+
+
+def nsfw_eval_dataset(dataset_dir, weights_path, top_k=1):
+    """
+    Evaluate the nsfw dataset
+    :param dataset_dir: The nsfw dataset dir which contains tensorflow records file
+    :param weights_path: The pretrained nsfw model weights file path
+    :param top_k: calculate the top k accuracy
+    :return:
+    """
+    assert ops.exists(dataset_dir)
+
+    # set nsfw data feed pipline
+    test_dataset = nsfw_data_feed_pipline.NsfwDataFeeder(dataset_dir=dataset_dir,
+                                                         flags='test')
+    prediciton_map = test_dataset.prediction_map
+    class_names = ['drawing', 'hentai', 'neural', 'porn', 'sexy']
+
+    with tf.device('/gpu:1'):
+        # set nsfw classification model
+        phase = tf.constant('test', dtype=tf.string)
+
+        # set nsfw net
+        nsfw_net = nsfw_classification_net.NSFWNet(phase=phase,
+                                                   resnet_size=CFG.NET.RESNET_SIZE)
+
+        # compute train loss
+        images, labels = test_dataset.inputs(batch_size=CFG.TEST.BATCH_SIZE,
+                                             num_epochs=1)
+
+        logits = nsfw_net.inference(input_tensor=images,
+                                    name='nsfw_cls_model',
+                                    reuse=False)
+
+        predictions = tf.nn.softmax(logits)
+
+    # Restore the moving average version of the learned variables for eval.
+    variable_averages = tf.train.ExponentialMovingAverage(
+        CFG.TRAIN.MOVING_AVERAGE_DECAY)
+    variables_to_restore = variable_averages.variables_to_restore()
+
+    # set tensorflow saver
+    saver = tf.train.Saver(variables_to_restore)
+
+    # Set sess configuration
+    sess_config = tf.ConfigProto(allow_soft_placement=True)
+    sess_config.gpu_options.per_process_gpu_memory_fraction = CFG.TEST.GPU_MEMORY_FRACTION
+    sess_config.gpu_options.allow_growth = CFG.TEST.TF_ALLOW_GROWTH
+    sess_config.gpu_options.allocator_type = 'BFC'
+
+    sess = tf.Session(config=sess_config)
+
+    # labels overall test dataset
+    labels_total = []
+    # prediction result overall test dataset
+    predictions_total = []
+    # prediction score overall test dataset of all subclass
+    predictions_prob_total = []
+
+    with sess.as_default():
+
+        saver.restore(sess=sess, save_path=weights_path)
+
+        while True:
+            try:
+                predictions_vals, labels_vals = sess.run(
+                    fetches=[predictions,
+                             labels])
+
+                log.info('**************')
+                log.info('Test dataset batch size: {:d}'.format(predictions_vals.shape[0]))
+                log.info('---- Sample Id ---- Gt label ---- Prediction ----')
+
+                for index, predictions_val in enumerate(predictions_vals):
+
+                    label_gt = prediciton_map[labels_vals[index]]
+
+                    prediction_score = dict()
+
+                    for score_index, score in enumerate(predictions_val):
+                        prediction_score[prediciton_map[score_index]] = format(score, '.5f')
+
+                    log.info('---- {:d} ---- {:s} ---- {}'.format(index, label_gt, prediction_score))
+
+                    # record predicts prob map
+                    predictions_prob_total.append(predictions_val.tolist())
+
+                # record total label and prediction results
+                labels_total.extend(labels_vals.tolist())
+                predictions_total.extend(np.argmax(predictions_vals, axis=1).tolist())
+
+            except tf.errors.OutOfRangeError as err:
+                log.info('Loop overall the test dataset')
+                break
+            except Exception as err:
+                log.error(err)
+                break
+
+    # calculate confusion matrix
+    cnf_matrix = confusion_matrix(labels_total, predictions_total)
+    np.set_printoptions(precision=2)
+    plot_confusion_matrix(cnf_matrix, classes=class_names, normalize=True,
+                          title='Normalized confusion matrix')
+
+    # calculate evaluate statics
+    calculate_evaluate_statics(labels=labels_total, predictions=predictions_total)
+
+    # plot precision recall curve
+    plot_precision_recall_curve(labels=labels_total,
+                                predictions_prob=predictions_prob_total,
+                                class_nums=5)
+    plt.show()
+
+    return
+
+
+if __name__ == '__main__':
+    # init args
+    args = init_args()
+
+    # test net
+    assert ops.exists(args.dataset_dir)
+
+    nsfw_eval_dataset(args.dataset_dir, args.weights_path)
--- tools/test_nsfw.py
+++ tools/test_nsfw.py
@@ -36,22 +36,43 @@ def init_args():
     :return:
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset_dir', type=str, default=None, help='The dataset dir')
     parser.add_argument('--image_path', type=str, default=None, help='The image path')
     parser.add_argument('--weights_path', type=str, help='The model weights file path')
 
     return parser.parse_args()
 
 
-def scale_image(image):
+def central_crop(image, central_fraction):
     """
 
     :param image:
+    :param central_fraction:
     :return:
     """
-    means = tf.expand_dims(tf.expand_dims(_CHANNEL_MEANS, 0), 0)
 
-    return image + means
+    image_shape = np.shape(image)
+    image_height = image_shape[0]
+    image_width = image_shape[1]
+
+    if central_fraction >= 1 or central_fraction < 0:
+        raise ValueError('Central fraction should be in [0, 1)')
+
+    top = int((1 - central_fraction) * image_height / 2)
+    bottom = image_height - top
+    left = int((1 - central_fraction) * image_width / 2)
+    right = image_width - left
+
+    if not image_height or not image_width:
+        raise ValueError('Image shape with zero')
+
+    if len(image_shape) == 2:
+        crop_image = image[top:bottom, left:right]
+        return crop_image
+    elif len(image_shape) == 3:
+        crop_image = image[top:bottom, left:right, :]
+        return crop_image
+    else:
+        raise ValueError('Wrong image shape')
 
 
 def calculate_top_k_error(predictions, labels, k=1):
@@ -69,103 +90,6 @@ def calculate_top_k_error(predictions, labels, k=1):
     return (batch_size - num_correct) / float(batch_size)
 
 
-def nsfw_eval_dataset(dataset_dir, weights_path, top_k=1):
-    """
-    Evaluate the nsfw dataset
-    :param dataset_dir: The nsfw dataset dir which contains tensorflow records file
-    :param weights_path: The pretrained nsfw model weights file path
-    :param top_k: calculate the top k accuracy
-    :return:
-    """
-    assert ops.exists(dataset_dir)
-
-    # set nsfw data feed pipline
-    test_dataset = nsfw_data_feed_pipline.NsfwDataFeeder(dataset_dir=dataset_dir,
-                                                         flags='test')
-    prediciton_map = test_dataset.prediction_map
-
-    with tf.device('/gpu:1'):
-        # set nsfw classification model
-        phase = tf.constant('test', dtype=tf.string)
-
-        # set nsfw net
-        nsfw_net = nsfw_classification_net.NSFWNet(phase=phase,
-                                                   resnet_size=CFG.NET.RESNET_SIZE)
-
-        # compute train loss
-        images, labels = test_dataset.inputs(batch_size=CFG.TEST.BATCH_SIZE,
-                                             num_epochs=1)
-
-        images_scale = tf.map_fn(fn=scale_image, elems=images, dtype=tf.float32)
-
-        logits = nsfw_net.inference(input_tensor=images,
-                                    name='nsfw_cls_model',
-                                    reuse=False)
-
-        predictions = tf.nn.softmax(logits)
-        top_k_error = calculate_top_k_error(predictions, labels, top_k)
-
-    # Restore the moving average version of the learned variables for eval.
-    variable_averages = tf.train.ExponentialMovingAverage(
-        CFG.TRAIN.MOVING_AVERAGE_DECAY)
-    variables_to_restore = variable_averages.variables_to_restore()
-
-    # set tensorflow saver
-    saver = tf.train.Saver(variables_to_restore)
-
-    # Set sess configuration
-    sess_config = tf.ConfigProto(allow_soft_placement=True)
-    sess_config.gpu_options.per_process_gpu_memory_fraction = CFG.TEST.GPU_MEMORY_FRACTION
-    sess_config.gpu_options.allow_growth = CFG.TEST.TF_ALLOW_GROWTH
-    sess_config.gpu_options.allocator_type = 'BFC'
-
-    sess = tf.Session(config=sess_config)
-
-    avg_prediction_top_k_accuracy = []
-
-    with sess.as_default():
-
-        saver.restore(sess=sess, save_path=weights_path)
-
-        while True:
-            try:
-                images_vals, predictions_vals, labels_vals, top1_error_val = sess.run(
-                    fetches=[images_scale,
-                             predictions,
-                             labels,
-                             top_k_error])
-
-                log.info('\n**************')
-                log.info('Test dataset batch size: {:d}'.format(images_vals.shape[0]))
-                log.info('---- Sample Id ---- Gt label ---- Prediction ----')
-
-                for index, image in enumerate(images_vals):
-
-                    label_gt = prediciton_map[labels_vals[index]]
-
-                    prediction_score = dict()
-
-                    for score_index, score in enumerate(predictions_vals[index]):
-                        prediction_score[prediciton_map[score_index]] = format(score, '.5f')
-
-                    log.info('---- {:d} ---- {:s} ---- {}'.format(index, label_gt, prediction_score))
-
-                    # plt.ion()
-                    # plt.figure('source image')
-                    # plt.imshow(np.array(image[:, :, (2, 1, 0)], np.uint8))
-                    # plt.pause(5.0)
-                    # plt.show()
-                    # plt.ioff()
-
-                log.info('Top 1 accuracy of this batch is: {:.5f}'.format(1 - top1_error_val))
-                avg_prediction_top_k_accuracy.append(1 - top1_error_val)
-
-            except tf.errors.OutOfRangeError as err:
-                log.info('Total avg top 1 accuracy is: {:.5f}'.format(np.mean(avg_prediction_top_k_accuracy)))
-                break
-    return
-
-
 def nsfw_classify_image(image_path, weights_path):
     """
     Use nsfw model to classify a single image
@@ -181,7 +105,7 @@ def nsfw_classify_image(image_path, weights_path):
         # set nsfw classification model
 
         image_tensor = tf.placeholder(dtype=tf.float32,
-                                      shape=[1, CFG.TRAIN.IMG_HEIGHT, CFG.TRAIN.IMG_WIDTH, 3],
+                                      shape=[1, CFG.TRAIN.CROP_IMG_HEIGHT, CFG.TRAIN.CROP_IMG_WIDTH, 3],
                                       name='input_tensor')
         # set nsfw net
         phase = tf.constant('test', dtype=tf.string)
@@ -220,6 +144,8 @@ def nsfw_classify_image(image_path, weights_path):
         image = cv2.resize(src=image,
                            dsize=(CFG.TRAIN.IMG_WIDTH, CFG.TRAIN.IMG_HEIGHT),
                            interpolation=cv2.INTER_CUBIC)
+        image = central_crop(image=image,
+                             central_fraction=CFG.TRAIN.CROP_IMG_HEIGHT / CFG.TRAIN.IMG_HEIGHT)
         image = np.array(image, dtype=np.float32) - np.array(_CHANNEL_MEANS, np.float32)
 
         predictions_vals = sess.run(
@@ -245,9 +171,6 @@ if __name__ == '__main__':
     args = init_args()
 
     # test net
-    if args.dataset_dir is not None and args.image_path is None:
-        nsfw_eval_dataset(args.dataset_dir, args.weights_path)
-    elif args.dataset_dir is None and args.image_path is not None:
-        nsfw_classify_image(args.image_path, args.weights_path)
-    else:
-        raise ValueError('You can not test a single image and test the nsfw dataset at the meantime')
+    assert ops.exists(args.image_path)
+
+    nsfw_classify_image(args.image_path, args.weights_path)
